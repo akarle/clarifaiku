1376806317f31cde3bba895c49e283022c4e48d3
Sandeep Dcunha <sdcunha@umass.edu>
2016-10-08 08:47:13 -0400

    Working file adding system. Put files in haiku directory on top level of repo. Each folder in the haiku dir should be a different class, represented by the foldername. It doesn't matter how many files there are in each directory or how make haiku there are in each file as long as each haiku is separated by at least one blank line from the previous.

--- clarifaiku/haiku_nn/models.py
+++ clarifaiku/haiku_nn/models.py
@@ -10,7 +10,13 @@ class Haiku(models.Model):
     line1 = models.CharField(max_length=30)
     line2 = models.CharField(max_length=30)
     line3 = models.CharField(max_length=30)
+    class Meta:
+        unique_together = ('line1', 'line2', 'line3')
 
-
+    def __str__(self):
+        return '%s\n%s\n%s\n%s' %(self.theme, self.line1, self.line2, self.line3)
 class Theme(models.Model):
-    theme = models.CharField(max_length=30)
+    theme = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.theme
