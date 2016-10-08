2906bb3788c066269cd7d76f0755528ccb0a7b1c
Sandeep Dcunha <sdcunha@umass.edu>
2016-10-08 05:27:04 -0400

    Started function to add haikus to db.

--- clarifaiku/haiku_nn/models.py
+++ clarifaiku/haiku_nn/models.py
@@ -3,3 +3,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Haiku(models.Model):
+    theme = models.ForeignKey('Theme', on_delete=models.CASCADE)
+    line1 = models.CharField(max_length=30)
+    line2 = models.CharField(max_length=30)
+    line3 = models.CharField(max_length=30)
+
+
+class Theme(models.Model):
+    theme = models.CharField(max_length=30)
--- clarifaiku/haiku_nn/views.py
+++ clarifaiku/haiku_nn/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import clarifaiku.haiku_nn.learn
+from clarifaiku.haiku_nn.models import *
 # Create your views here.
 
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the haiku_nn index.")
+    return HttpResponse('test')
+
+
+def addHaiku(theme, text):
+    m_theme = Theme(theme=theme)
+    m_theme.save()
+    m_haiku = Haiku(theme=theme)
+
