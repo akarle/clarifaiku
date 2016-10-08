988816f72d4530fb2751bf7f12876d516012cb2f
Shane Parr <sparr@umass.edu>
2016-10-08 08:03:14 -0400

    add skip-gram model

--- clarifaiku/haiku_nn/learn.py
+++ clarifaiku/haiku_nn/learn.py
@@ -1,3 +1,114 @@
 import tensorflow as tf
+import math
+import random
+import collections
+import numpy as np
+import re
+datastring = """
+You never feed me.
+Perhaps I'll sleep on your face.
+That will sure show you.
+You're always typing.
+Well, let's see you ignore my
+sitting on your hands. In deep sleep hear sound
+cat vomit hairball somewhere
+will find in"""
+re_word = re.compile(r"[a-zA-Z']+")
+words = re.findall(re_word, datastring.lower())
+vocabulary_size = len(words)
+embedding_size = vocabulary_size
+batch_size = vocabulary_size
+#print vocabulary_size
+num_sampled = vocabulary_size/5
+#print num_sampled
+num_skips = 2
+skip_window = 1
+sess = tf.InteractiveSession()
+data_index = 0
+
+
+def build_dataset(words):
+    count = [['UNK', -1]]
+    count.extend(collections.Counter(words).most_common(vocabulary_size - 1))
+    dictionary = dict()
+    for word, _ in count:
+        dictionary[word] = len(dictionary)
+    data = list()
+    unk_count = 0
+    for word in words:
+        if word in dictionary:
+          index = dictionary[word]
+        else:
+          index = 0  # dictionary['UNK']
+          unk_count += 1
+        data.append(index)
+    count[0][1] = unk_count
+    reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+    return data, count, dictionary, reverse_dictionary
+data, count, dictionary, reverse_dictionary = build_dataset(words)
+print('Most common words (+UNK)', count[:6])
+print('Sample data', data[:10], [reverse_dictionary[i] for i in data[:10]])
+
+def generate_batch(batch_size, num_skips, skip_window):
+    global data_index
+    assert batch_size % num_skips == 0
+    assert num_skips <= 2 * skip_window
+    batch = np.ndarray(shape=batch_size, dtype=np.int32)
+    labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
+    span = 2 * skip_window + 1  # [ skip_window target skip_window ]
+    buffer = collections.deque(maxlen=span)
+    for _ in range(span):
+        buffer.append(data[data_index])
+        data_index = (data_index + 1) % len(data)
+    for i in range(batch_size // num_skips):
+        target = skip_window  # target label at the center of the buffer
+        targets_to_avoid = [skip_window]
+        for j in range(num_skips):
+            while target in targets_to_avoid:
+                target = random.randint(0, span - 1)
+            targets_to_avoid.append(target)
+            batch[i * num_skips + j] = buffer[skip_window]
+            labels[i * num_skips + j, 0] = buffer[target]
+    buffer.append(data[data_index])
+    data_index = (data_index + 1) % len(data)
+    return batch, labels
+
+batch, labels = generate_batch(batch_size=8, num_skips=2, skip_window=1)
+for i in range(8):
+    print(batch[i], reverse_dictionary[batch[i]], '->', labels[i, 0], reverse_dictionary[labels[i, 0]])
+
+train_inputs = tf.placeholder(tf.int32, shape=[batch_size])
+train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
+
+embeddings = tf.Variable(
+    tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
+embed = tf.nn.embedding_lookup(embeddings, train_inputs)
+
+nce_weights = tf.Variable(
+  tf.truncated_normal([vocabulary_size, embedding_size],
+                      stddev=1.0 / math.sqrt(embedding_size)))
+nce_biases = tf.Variable(tf.zeros([vocabulary_size]))
+
+loss = tf.reduce_mean(
+  tf.nn.nce_loss(nce_weights, nce_biases, embed, train_labels,
+                 num_sampled, vocabulary_size))
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=1.0).minimize(loss)
+
+init = tf.initialize_all_variables()
+# the ugly
+
+num_steps = vocabulary_size*2+1
+
+init.run()
+average_loss = 0
+for step in xrange(num_steps):
+    batch_inputs, batch_labels = generate_batch(
+        batch_size, num_skips, skip_window)
+    feed_dict = {train_inputs: batch_inputs, train_labels: batch_labels}
+    _, loss_val = sess.run([optimizer, loss], feed_dict=feed_dict)
+    average_loss += loss_val
+print average_loss/num_steps
+
 
 
