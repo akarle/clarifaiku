a1f892c73c566a16647c4baa70e8dc4ea77fbfe3
Alexander Karle <akarle@umass.edu>
2016-10-08 04:47:04 -0400

    added index.html

--- clarifaiku/porch/views.py
+++ clarifaiku/porch/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the porch index.")
+    template = loader.get_template('porch/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
